be8a00e2e6b3478e45640a62898d4ae563210e85
Sam Burnstone <sburnstone@scottlogic.com>
2020-01-13 16:06:07 +0000

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  text-align: center;
+`;
+
+const StyledHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(0px + 2vmin);
+  color: white;
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
+    <StyledApp>
+      <StyledHeader>
         <h1>Shower Logic</h1>
-      </header>
-    </div>
+        <input type="text" />
+      </StyledHeader>
+    </StyledApp>
   );
 }
 
