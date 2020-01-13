6b298972c62c393ae1ee0f6f55ab0399abe5a112
Sam Burnstone <sburnstone@Sam-Bs-Mac-mini.local>
2020-01-13 15:26:21 +0000

    Begin replacing CRA content

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app name', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appTitle = getByText(/Shower Logic/i);
+  expect(appTitle).toBeInTheDocument();
 });
