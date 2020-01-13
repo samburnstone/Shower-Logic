9cea90a38a3522d3ff1636f1d828a21f50c16a73
Sam Burnstone <sburnstone@scottlogic.com>
2020-01-13 16:16:32 +0000

    Add styled components for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,35 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 
 const StyledApp = styled.div`
   text-align: center;
 `;
 
 const StyledHeader = styled.header`
-  background-color: #282c34;
+  background-color: ${props => props.theme.blue};
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  font-size: calc(0px + 2vmin);
-  color: white;
+  font-size: calc(10px + 2vmin);
+  color: ${props => props.theme.white};
 `;
+
+const theme = {
+  blue: '#065172',
+  white: '#F3F3F3'
+}
 
 const App: React.FC = () => {
   return (
-    <StyledApp>
-      <StyledHeader>
-        <h1>Shower Logic</h1>
-        <input type="text" />
-      </StyledHeader>
-    </StyledApp>
+    <ThemeProvider theme={theme}>
+      <StyledApp>
+        <StyledHeader>
+          <h1>Shower Logic</h1>
+        </StyledHeader>
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
